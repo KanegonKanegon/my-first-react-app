489fef509d17e73e9aba786d2765e910a21194c6
1F10230244 <s1f102302449@iniad.org>
2025-06-02 16:44:38 +0900

    増幅幅の設定

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import React, { useState } from 'react';
 const App: React.FC = () => {
   // stateの宣言: countは数字型(number)
   const [count, setCount] = useState<number>(0);
+  const [step, setStep] = useState<number>(1);
 
   // カウントを増やす関数
-  const increment = () => setCount(count + 1);
+  const increment = () => setCount(count + step);
 
   // カウントを減らす関数
-  const decrement = () => setCount(count - 1);
+  const decrement = () => setCount(count - step);
 
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
@@ -16,6 +17,16 @@ const App: React.FC = () => {
       <p>カウント: {count}</p>
       <button onClick={decrement}>− 減らす</button>
       <button onClick={increment} style={{ marginLeft: '10px' }}>＋ 増やす</button>
+      <div>
+        <p>増幅幅の設定</p>
+        <input 
+        type="number" //入力できる値を数字限定に
+        value={step}
+        onChange={(e) => setStep(Number(e.target.value))}
+        style={{marginLeft:'10px', width: '50px'}}
+        min="1"
+        /> 
+      </div>
     </div>
   );
 };
