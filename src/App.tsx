189ccb96fb0246a7e51fa1ea7f55c8e3e782581a
1F10230244 <s1f102302449@iniad.org>
2025-07-01 16:00:26 +0900

    善行アプリ開発_善行記録を設定、表示

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,29 @@ const App: React.FC = () => {
   const [step, setStep] = useState<number>(1);
   const [countstar, setCountstar] = useState<number>(0);
   const [errorstar, setErrorstar] = useState<string>("");
+  // 善行の内容
+  const [goodeed, setGoodeed] = useState<string>("");
+  // 善行の記録を設定
+  const [deeds, setDeeds] = useState<GoodDeed[]>([]);
+
+  // 善行記録の型を自分で作る
+  type GoodDeed ={
+    content: string;
+    point: number;
+    date: string;
+  };
+
+  const addDeed = () => {
+    const today = new Date().toISOString().split("T")[0];
+    const newdeed: GoodDeed={
+      content: goodeed,
+      point: count, 
+      date: today,
+    };
+    setDeeds([...deeds, newdeed]);
+    setGoodeed("");
+    setCount(0);
+  };
 
   // カウントを増やす
   const increment = () => setCount(count + step);
@@ -26,11 +49,20 @@ const App: React.FC = () => {
 
 
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+    <div style={{ textAlign: 'center', marginTop: '50px', paddingBottom: '150px'}}>
       <h1>善行カウンターアプリ</h1>
+      <p>累計善行ポイント: </p>
+      <p>善行の内容: {goodeed}</p>
       <p>善行カウント: {count}</p>
       <button onClick={decrement}>− 減らす</button>
       <button onClick={increment} style={{ marginLeft: '10px' }}>＋ 増やす</button>
+      <div>
+        <p>善行の内容を表記</p>
+        <input
+        type="text"
+        value={goodeed}
+        onChange={(e) => setGoodeed(e.target.value)}></input>
+      </div>
       <div>
         <p>増幅幅の設定</p>
         <input 
@@ -41,6 +73,20 @@ const App: React.FC = () => {
         min="1"
         /> 
       </div>
+      <br></br>
+      <div>
+        <button onClick={addDeed}>善行を記録する！</button>
+      </div>
+      <div>
+        <p>善行一覧</p>
+        {deeds.map((d, i) => (
+          <div key={i} style={{ border: '1px solid #ccc', margin: '10px', padding: '10px'}}>
+            <p>日付: {d.date}</p>
+            <p>内容: {d.content}</p>
+            <p>ポイント: {d.point}</p>
+          </div>
+        ))}
+      </div>
       <div>
         <h1>このアプリの評価(1~5段階)</h1>
         <input
@@ -48,10 +94,13 @@ const App: React.FC = () => {
         value={countstar}
         onChange={(e) => {
           const val=Number(e.target.value)
-          if (!isNaN(val) && val<=5 && val>0) {
+          if (!isNaN(val) && val>0 && val<=5) {
             setErrorstar("");
             setCountstar(val);
-          } else{
+          } else if (val>5) {
+            setErrorstar("気持ちは受け取っとくけどね？");
+            setCountstar(1);
+          } else {
             setErrorstar("正しい数字入れろバーカ");
           }
         }}
